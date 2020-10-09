294edfe1d68f7bb5947f48e0f97a10649968cb6c
l30001819 <lantianyun@huawei.com>
2020-10-09 20:23:55 +0800

    两数平方和，两数和，翻转元音字母

--- 双指针/345_reverseVowels.py
+++ 双指针/345_reverseVowels.py
@@ -3,3 +3,32 @@
 # @Time : 2020/10/9 19:36
 # @Author : lantianyun l30001819
 # @File : 345_reverseVowels.py
+
+# 翻转字符串中的元音字母
+
+
+class Solution:
+    @classmethod
+    def reverseVowels(self, s: str) -> str:
+
+        str = list(s)
+        head = 0
+        tail = len(str)-1
+
+        while head < tail:
+            if str[head].lower() in ('a','e','i','o','u') and str[tail].lower() in ('a','e','i','o','u'):
+                str[head],str[tail] = str[tail],str[head]
+                head += 1
+                tail -= 1
+            elif str[head].lower() in ('a','e','i','o','u'):
+                tail -= 1
+            elif str[tail].lower() in ('a','e','i','o','u'):
+                head += 1
+            else:
+                head += 1
+                tail -= 1
+
+        return ''.join(str)
+
+
+print(Solution.reverseVowels("leetcode"))
---
+++ 双指针/validPalindrome.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2020/10/9 20:23
+# @Author : lantianyun l30001819
+# @File : validPalindrome.py
