d1cc39dd13de5248437e72b2ad6e05ba5de5da4b
l30001819 <lantianyun@huawei.com>
2020-10-16 19:38:22 +0800

    csv多行处理

---
+++ csv/csv_op.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2020/10/16 18:04
+# @Author : lantianyun l30001819
+# @File : csv_op.py
+
+
+import csv
+content = open("1275442.csv", "r",encoding='UTF-8').read().replace('\\"',chr(1))
+with open("1275442.csv.new", "w",encoding='UTF-8') as g:
+    g.write(content)
+with open('1275442.csv.new','r',encoding='UTF-8') as csvIN:
+   for row in csv.reader(csvIN, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL):
+       print(row)
