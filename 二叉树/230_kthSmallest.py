ee9b4ea74f108b036481787d9bfd1b9fb06d8d21
l30001819 <lantianyun@huawei.com>
2020-10-26 16:11:33 +0800

    二叉搜索树：树的实现和删除、裁剪

---
+++ 二叉树/230_kthSmallest.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time : 2020/10/26 16:11
+# @Author : lantianyun l30001819
+# @File : 230_kthSmallest.py
--- 二叉树/669_trimBST.py
+++ 二叉树/669_trimBST.py
@@ -41,16 +41,64 @@ class BST:
             self.root=self.insert(self.root,num)
         return self.root
 
+    def delete(self,root, val):
+        """二叉搜索树的删除"""
+        """删除有三种情况：
+        1. 节点没有左孩子，也没有右孩子，也就是自己是树里面的叶子节点
+        2. 节点只有左孩子或者右孩子（即只有一边）
+        3. 节点既有左孩子，也有右孩子
+        """
 
-    def layerScan(self):
+        # 先定义一个找最小节点的函数
+        def findMin(root):
+            if root.left:
+                return findMin(root.left)
+            else:
+                return root
+
+
+        if not root:   # 空树没有左右孩子，要提前判断
+            return None
+
+        # 1. 节点没有左孩子，也没有右孩子
+        # 直接删除自己，返回空
+        if root.val == val and root.left is None and root.right is None:
+            return None
+
+        # 2. 节点只有左孩子或者右孩子
+        # 删除自己，然后把左孩子或者右孩子顶上去
+        if root.val == val and root.left and root.right is None:
+            return root.left
+        if root.val == val and root.left is None and root.right:
+            return root.right
+
+        # 3. 节点既有左孩子，又有右孩子
+        # 在该节点的右子树中找最小的（也就是右子树中最左的），替代本节点（所谓替代，其实做法是把那个节点的值拷贝到本节点，然后再删除那个节点）
+        if root.val == val and root.left and root.right:
+            min_of_right = findMin(root.right)
+            root.val = min_of_right.val  # 直接通过赋值操作来替换，这样就不需要动树结构
+            root.right = self.delete(root.right, min_of_right.val) # 然后再从右树中删除那个值(注意右树中删除以后，要把右树的根接上来，所以要root.right=)
+            return root # 注意不是返回右树，而是返回自己
+
+
+        # 接下来就是正常逻辑了，上面都是找到了需要删除的值，如果没找到，就得继续遍历左右子树
+        if root.val < val: # 如果root的值小于要删除的值，就在右子树中找
+            root.right = self.delete(root.right, val)
+            return root
+        elif root.val > val: # 否则在左子树中找
+            root.left = self.delete(root.left, val)
+            return root
+
+
+    def layerScan(self,root):
         """二叉搜索树的层序遍历"""
         """层序遍历的结果可以直接在力扣669中进行树结构可视化"""
         from collections import deque  # 引入双端队列，目的是层序遍历（又叫广度遍历BFS）
         res = []
         q = deque()
-        if not self.root:
+        if not root:
             return res
-        q.append(self.root)
+        q.append(root)
 
         while q:
             cur_node = q.popleft()
@@ -94,7 +142,7 @@ class Solution:
             return self.trimBST(root.left, low, high)
         else:
             # 接下来就是本节点满足值域范围的情况，那就是直接把自己的左孩子设置成左树最终的根，也就是跟上面进行联动
-            root.left = self.trimBST( root.left, low, high)
+            root.left = self.trimBST(root.left, low, high)
             # 右边一样
             root.right = self.trimBST(root.right, low, high)
             # 惯例返回自己
@@ -102,11 +150,14 @@ class Solution:
 
 
 if __name__ == "__main__":
-    bst = BST(nums=[1,2,3,4],root=None)
-    print(bst.layerScan())
+    bst = BST(nums=[11,3,2,5,15],root=None)
+    print(bst.layerScan(bst.delete(bst.root, 3)))
 
-    s = Solution()
-    root = s.trimBST(bst.root,0,0)
 
-    bst2 = BST(nums=None, root=root)
-    print(bst.layerScan())
+
+
+    # s = Solution()
+    # root = s.trimBST(bst.root,0,0)
+    #
+    # bst2 = BST(nums=None, root=root)
+    # print(bst.layerScan())
